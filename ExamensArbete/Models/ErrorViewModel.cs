95a707843203f6700748269d7923b0fd698a438b
Iman-53 <112899355+Iman-53@users.noreply.github.com>
2023-11-06 10:48:13 +0100

    Add project files.

---
+++ ExamensArbete/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamensArbete.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
